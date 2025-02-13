c7a43f07cc2222deae38e9fd525f41a96c26d44d
justinelouiealojado <justinealojado513@gmail.com>
2025-02-13 16:22:48 +0800

    Submitting ADVDBMS Week 02 - Session 01 - Exercise 01

--- drop_db.sql
+++ drop_db.sql
@@ -1,6 +1,6 @@
 -- Step 3: (Optional) Drop the database if needed. 
 -- Use with extreme caution!
-USE `Universitydb`;
+USE `UniversityDB`;
 
 -- The IF EXISTS clause prevents an error if the database doesn't exist.
-DROP DATABASE IF EXISTS `Universitydb`;
+DROP DATABASE IF EXISTS `UniversityDB`;

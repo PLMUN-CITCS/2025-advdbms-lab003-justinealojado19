c9e8d20b14d6b0e2e04fd791732e983659031970
justinelouiealojado <justinealojado513@gmail.com>
2025-02-25 21:59:47 +0800

    Delete university_db directory

--- university_db/create_and_use_db.sql
+++
@@ -1,6 +0,0 @@
--- Step 2: Use the newly created database
-USE `universitydb`;
-
--- Verification (Optional): 
--- You can add a simple query to verify the database is selected.
-SELECT DATABASE(); -- This will show the currently selected database.

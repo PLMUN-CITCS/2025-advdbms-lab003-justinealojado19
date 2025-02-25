4b458456238112843ad0caf038564e61ac70f90d
justinelouiealojado <justinealojado513@gmail.com>
2025-02-25 23:26:47 +0800

    "Submitting ADVDBMS Week 02 - Session 01 - Exercise 01"

--- university_db/create_and_use_db.sql.sql
+++ university_db/create_and_use_db.sql.sql
@@ -1,29 +1,9 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Feb 25, 2025 at 04:06 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Step 1: Create a database called UniversityDB
+CREATE DATABASE `UniversityDB`;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+-- Step 2: Use the newly created database
+USE `UniversityDB`;
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `universitydb`
---
-CREATE DATABASE IF NOT EXISTS `universitydb` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `universitydb`;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- Verification (Optional): 
+-- You can add a simple query to verify the database is selected.
+SELECT DATABASE(); -- This will show the currently selected database.
